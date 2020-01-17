28709852ed7a098ff5d1d0d3bc817c1fa5ac8cc7
Sabbir Ahmmed <59827355+Sabbir185@users.noreply.github.com>
2020-01-17 11:38:48 +0600

    1155 - S Sequence

---
+++ URI_1155.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   float sum=0,i;
+        for (i = 1; i <=100 ; i++) {
+            sum = sum + (1/i);
+        }
+        printf("%.2f\n",sum);
+ 
+    return 0;
+}
