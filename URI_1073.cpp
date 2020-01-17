9469d4adf3ffdb077fbf5acf9f1de5752dfc3892
Sabbir Ahmmed <59827355+Sabbir185@users.noreply.github.com>
2020-01-17 19:09:18 +0600

    1073 - Even Square

---
+++ URI_1073.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    if( n>5 && n<2000)
+    {
+        for( int i=1; i<=n; i++ )
+        {
+
+            if( i%2==0 )
+            {
+                cout<<i<<"^2 = "<<i*i<<endl;
+            }
+        }
+    }
+}
