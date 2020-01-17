fff13515bc9407c3a33cbdba945505ec8aa6f930
Sabbir Ahmmed <59827355+Sabbir185@users.noreply.github.com>
2020-01-17 19:13:15 +0600

    1071 - Sum of Consecutive Odd Numbers I

---
+++ URI_1071.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x,y,sum=0;
+    cin>>x>>y;
+    if( x<y )
+    {
+        for( int i=x+1; i<y; i++ )
+        {
+            if( i%2==1 || i%2==-1)
+            {
+                sum+=i;
+            }
+        }
+    }
+    else
+    {
+        for( int i=y+1; i<x; i++)
+        {
+            if( i%2==1 || i%2==-1 )
+            {
+                 sum+=i;
+            }
+        }
+    }
+    cout<<sum<<endl;
+}
