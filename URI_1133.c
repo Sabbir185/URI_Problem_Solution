2643ca9d0a6a646c56537b0d08f17dff3ad7afd6
Sabbir Ahmmed <59827355+Sabbir185@users.noreply.github.com>
2020-01-17 16:24:59 +0600

    1133 - Rest of a Division

---
+++ URI_1133.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int x,y,i,j;
+    scanf("%d\n",&x);
+    scanf("%d\n",&y);
+
+        if (x>y)
+        {
+            for( i=y+1; i<x; i++ )
+            {
+                if ( i%5==2 || i%5==3 )
+                {
+                    printf("%d\n",i);
+                }
+            }
+        }
+        else if (x<y)
+        {
+            for( j=x+1; j<y; j++ )
+            {
+                if ( j%5==2 || j%5==3 )
+                {
+                    printf("%d\n",j);
+                }
+            }
+        }
+ 
+    return 0;
+}
