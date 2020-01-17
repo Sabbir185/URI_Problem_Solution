4e726b888232774b9099c34057f4fd23f071d393
Sabbir Ahmmed <59827355+Sabbir185@users.noreply.github.com>
2020-01-17 18:28:23 +0600

    1114 - Fixed Password

---
+++ URI_1114.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int x;
+	while (1) {
+		cin>>x;
+		if (x==2002) {
+			cout<<"Acesso Permitido"<<endl;
+			break;
+		}
+		else
+			cout<<"Senha Invalida"<<endl;
+		}
+ 
+    return 0;
+}
