c2d70387723fc70dc3a773666e5721f046d7493a
Sabbir Ahmmed <59827355+Sabbir185@users.noreply.github.com>
2020-01-18 00:41:25 +0600

    1001 - Extremely Basic

---
+++ URI_1001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		int X = A + B ;
+		System.out.println("X = "+X);
+
+	}
+
+}
