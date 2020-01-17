7febfbdd6c5889373afe19c6121f868cd1e68000
Sabbir Ahmmed <59827355+Sabbir185@users.noreply.github.com>
2020-01-18 00:24:55 +0600

    1016 - Distance

---
+++ URI_1016.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+ 
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+		  Scanner scanner = new Scanner(System.in);
+		  
+		  int x = scanner.nextInt();
+		  
+		  int time = x * 2 ;
+		  
+		  System.out.println(time+" minutos");
+    }
+ 
+}
