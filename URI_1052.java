b1da095239ec92436d3d9f90f4fa58c13ae7e767
Sabbir Ahmmed <59827355+Sabbir185@users.noreply.github.com>
2020-01-17 19:54:23 +0600

    1052 - Month

---
+++ URI_1052.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		
+		switch (month) {
+		case 1:
+			System.out.printf("January\n");
+			break;
+		case 2:
+			System.out.printf("February\n");
+			break;
+		case 3:
+			System.out.printf("March\n");
+			break;
+		case 4:
+			System.out.printf("April\n");
+			break;
+		case 5:
+			System.out.printf("May\n");
+			break;
+		case 6:
+			System.out.printf("June\n");
+			break;
+		case 7:
+			System.out.printf("July\n");
+			break;
+		case 8:
+			System.out.printf("August\n");
+			break;
+		case 9:
+			System.out.printf("September\n");
+			break;
+		case 10:
+			System.out.printf("October\n");
+			break;
+		case 11:
+			System.out.printf("November\n");
+			break;
+			
+		default:
+			System.out.printf("December\n");
+			break;
+		}
+		
+ 
+    }
+ 
+}
